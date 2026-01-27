2b2c4985c6d53f3bc3486802d61b68b6f0053199
rezwankhandakar <rezwankhandaker02@gmail.com>
2026-01-27 19:35:46 +0600

    navbar

---
+++ src/Pages/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h2 className='text-5xl'>Error Page</h2>
+        </div>
+    );
+};
+
+export default ErrorPage;
